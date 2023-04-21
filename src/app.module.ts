c5fff54dd28e7b423c929a3a53b599221748b684
McKay <mckaybwarren@gmail.com>
2023-04-20 22:06:17 -0400

    reorganize application file structure

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,12 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { CatsController } from './cats/cats.controller';
+import { CatsController } from './common/controllers/cats.controller';
+import { CatsService } from './common/providers/cats.service';
 
 @Module({
   imports: [],
   controllers: [AppController, CatsController],
-  providers: [AppService],
+  providers: [AppService, CatsService],
 })
 export class AppModule {}
